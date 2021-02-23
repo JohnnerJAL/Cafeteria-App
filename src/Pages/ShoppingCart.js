c308c9798a0973efa74dcc151aeacb3c116139a9
Johnner Linux <johnner@outlook.com>
2021-02-23 16:56:24 -0500

    html & css app, screen Shopping Cart

--- src/Pages/ShoppingCart.js
+++ src/Pages/ShoppingCart.js
@@ -1,9 +1,100 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Styles/ShoppingCart.css";
+import hamburgerImage from "../Images/Hamburger-pexels.jpg";
+import empanadaImage from "../Images/Empanadas-pexels.jpg";
+import beerImage from "../Images/beer-pexels.jpg";
 
 function ShoppingCart() {
+
+  const hamburger = {
+    backgroundImage: `url('${hamburgerImage}')`
+  }
+  const empanada = {
+    backgroundImage: `url('${empanadaImage}')`
+  }
+  const beer = {
+    backgroundImage: `url('${beerImage}')`
+  }
+
+
   return (
     <section className="shopping-cart">
-      ShoppingCart
+      <div className="title">
+        <h1>Mi Carrito</h1>
+        <i class="fas fa-cart-arrow-down"></i>
+      </div>
+
+      <div className="message-container">
+        <div className="message">
+          <h1>Recuerda:</h1>
+          <p>Tu orden será enviada a nuestro whatsapp en donde coordinaremos la entrega y el pago</p>
+        </div>
+      </div>
+
+      <div className="articles">
+      {/* <div className="list-of-items"> */}
+        <article>
+          <figure style={empanada}></figure>
+          <div className="contents">
+            <h4>Empanada de carne</h4>
+            {/* <p>Empanada de carne con huevo</p> */}
+            <p>$ 1800</p>
+            <p>1</p>
+          </div>
+          <div className="button button-left">
+            <div className="circle">-</div>
+          </div>
+          <div className="button button-right">
+            <div className="circle">+</div>
+          </div>
+        </article>
+
+        <article>
+          <figure style={empanada}></figure>
+          <div className="contents">
+            <h4>Empanada de pollo</h4>
+            {/* <p>Empanada de pollo con huevo</p> */}
+            <p>$ 1800</p>
+            <p>1</p>
+          </div>
+          <div className="button button-left">
+            <div className="circle">-</div>
+          </div>
+          <div className="button button-right">
+            <div className="circle">+</div>
+          </div>
+        </article>
+      {/* </div> */}
+      </div>
+
+      <div className="resume">
+       <div>
+          <Link  to="/menu">
+            <p>Agregar más productos</p>
+            {/* <i class="fas fa-cart-arrow-down"></i> */}
+          </Link>
+        </div>
+
+      <div>
+        <div className="row">
+          <p>Envío (opcional)</p>
+          <p>$2 000</p>
+        </div>
+        <div className="row">
+          <p>Total</p>
+          <p>$5 600</p>
+        </div>
+      </div>
+
+      <div>
+        <Link  to="/shopping-cart">{/*¡?*/}
+          <p>Confirmar orden</p>
+          <i class="fab fa-whatsapp"></i>
+        </Link>
+      </div>
+      </div>
     </section>
   )
 }
